271890409d956566cccc5e8918ca8e0a9e5c94bf
Daniel Bair <daniel@danielbair.com>
2017-01-23 10:32:13 -0500

    Updated aeneas to v1.7.1 (#371)
    * Updated aeneas to v1.7.0
    
    * Update resource to current version..
    
    BeautifulSoup4 4.5.1
    
    * Update aeneas to v1.7.1 for upstream bugfixes
    
    * Fix for brew audit --strict

--- aeneas.rb
+++ aeneas.rb
@@ -1,8 +1,8 @@
 class Aeneas < Formula
   desc "Python/C library and set of tools to synchronize audio and text"
   homepage "http://www.readbeyond.it/aeneas/"
-  url "https://github.com/readbeyond/aeneas/archive/v1.5.1.0.tar.gz"
-  sha256 "349ee38c41ef8a3dda70e476185de0be51fede7e1bc2f9007923b58be524f25e"
+  url "https://github.com/readbeyond/aeneas/archive/v1.7.1.tar.gz"
+  sha256 "b49d80c4059d66f2a8bed114757fd10efc1f1ecff563da96be67149b3e6af83c"
   head "https://github.com/readbeyond/aeneas.git"
 
   bottle do
@@ -12,16 +12,16 @@ class Aeneas < Formula
     sha256 "6116f2c5fcf947ed4d6381f81af3e548f98cffa586faf1456fc2384084c39107" => :mavericks
   end
 
+  option "without-python", "Build without Python 2.7 support"
+
   depends_on "espeak" => :recommended
   depends_on "ffmpeg" => :recommended
   depends_on :python3 => :optional
   depends_on "homebrew/python/numpy"
 
-  option "without-python", "Build without Python 2.7 support"
-
   resource "beautifulsoup4" do
-    url "https://pypi.python.org/packages/26/79/ef9a8bcbec5abc4c618a80737b44b56f1cb393b40238574078c5002b97ce/beautifulsoup4-4.4.1.tar.gz"
-    sha256 "87d4013d0625d4789a4f56b8d79a04d5ce6db1152bb65f1d39744f7709a366b4"
+    url "https://pypi.python.org/packages/86/ea/8e9fbce5c8405b9614f1fd304f7109d9169a3516a493ce4f7f77c39435b7/beautifulsoup4-4.5.1.tar.gz"
+    sha256 "3c9474036afda9136aac6463def733f81017bf9ef3510d25634f335b0c87f5e1"
   end
 
   resource "lxml" do
